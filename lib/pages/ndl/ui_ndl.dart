035a1ae626dfe832d20a16c051d1b6c94b33b1ee
kevinxkiva <66553736+kevinxkiva@users.noreply.github.com>
2023-03-07 16:17:59 +0700

    07/03/2023 16:17

--- lib/pages/ndl/ui_ndl.dart
+++ lib/pages/ndl/ui_ndl.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:ndlproject_desktop/pages/widgets/textview.dart';
+import 'package:ndlproject_desktop/themes/colors.dart';
 
 class NdlPage extends StatefulWidget {
   const NdlPage({super.key});
@@ -8,10 +12,268 @@ class NdlPage extends StatefulWidget {
 }
 
 class _NdlPageState extends State<NdlPage> {
+  final List<DataRow> _rowList = List.empty(growable: true);
+
+  @override
+  void initState() {
+    _rowList.clear();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: ScrollConfiguration(
+      behavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse}),
+      child: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        controller: ScrollController(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                      child: SingleChildScrollView(
+                    physics: const ClampingScrollPhysics(),
+                    controller: ScrollController(),
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                        border: TableBorder.all(
+                          color: Colors.black.withOpacity(0.5),
+                          style: BorderStyle.solid,
+                        ),
+                        headingRowHeight: 50,
+                        dataRowHeight: 56,
+                        columns: [
+                          datCol("Tambahkan Data Tgl"),
+                          datCol("Customer Delivery Date"),
+                          datCol("Job Done"),
+                          datCol("Durasi"),
+                          datCol("Analyzer Version"),
+                          datCol("Order Status (New/Repeat)"),
+                          datCol("Cylinder Status"),
+                          datCol("Gol"),
+                          datCol("WS No"),
+                          datCol("Customer"),
+                          datCol("Item Nama"),
+                          datCol("Model"),
+                          datCol("Up"),
+                          datCol("Repeat"),
+                          datCol("Toleransi"),
+                          datCol("Order"),
+                          datCol("W/S Order (m)"),
+                          datCol("Width"),
+                          datCol("Length"),
+                          datCol("Gusset"),
+                          datCol("prod.size"),
+                          datCol("W"),
+                          datCol("C"),
+                          datCol("Color"),
+                          datCol("1st Layer"),
+                          datCol("Width1"),
+                          datCol("RM"),
+                          datCol("Diff"),
+                          datCol("2nd Layer"),
+                          datCol("Width1"),
+                          datCol("RM"),
+                          datCol("Diff"),
+                          datCol("3nd Layer"),
+                          datCol("Width1"),
+                          datCol("RM"),
+                          datCol("Diff"),
+                          datCol("4nd Layer"),
+                          datCol("Width1"),
+                          datCol("RM"),
+                          datCol("Diff"),
+                          datCol("5nd Layer"),
+                          datCol("Width1"),
+                          datCol("RM"),
+                          datCol("Diff"),
+                          datCol("6nd Layer"),
+                          datCol("Width1"),
+                          datCol("RM"),
+                          datCol("Diff"),
+                          datCol("1st Layer"),
+                          datCol("Ink"),
+                          datCol("adh1"),
+                          datCol("2nd Layer"),
+                          datCol("adh2"),
+                          datCol("3nd Layer"),
+                          datCol("adh3"),
+                          datCol("4nd Layer"),
+                          datCol("adh4"),
+                          datCol("5nd Layer"),
+                          datCol("adh5"),
+                          datCol("6nd Layer"),
+                          datCol("Total"),
+                          datCol("Aksi"),
+                        ],
+                        rows: [
+                          DataRow(
+                            cells: [
+                              datRow1("02-Jan-23"),
+                              datRow1("17-Jan-23"),
+                              datRow1("16-Jan-23"),
+                              datRow1("14"),
+                              datRow1("V.161"),
+                              datRow1("Repeat"),
+                              datRow1("Tidak Ada"),
+                              datRow1("S"),
+                              datRow1("0001"),
+                              datRow1("Reyner"),
+                              datRow1("Ikan Laut sALMON 70g"),
+                              datRow1("3SS + Sealed"),
+                              datRow1("1"),
+                              datRow1(""),
+                              datRow1("0"),
+
+                              datRow2("66.000", "60.000"),
+
+                              datRow1("15.180"),
+                              datRow1("0.370"),
+                              datRow1("0.280"),
+                              datRow1("0.070"),
+                              datRow1("0.740"),
+                              datRow1(""),
+                              datRow1(""),
+                              datRow1("8"),
+
+                              datRow4("PET", "15", "u", "1.16"),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("PE1", "80", "u", "0.92"),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow1("200.074"),
+                              datRow1("46.16"),
+                              datRow1("46.16"),
+                              datRow1("849.11"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow1("849.11"),
+                              datRow1("-"),
+                              datRow1("849.11"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow3()
+                            ],
+                          ),
+                          DataRow(
+                            cells: [
+                              datRow1("02-Jan-23"),
+                              datRow1("17-Jan-23"),
+                              datRow1("16-Jan-23"),
+                              datRow1("14"),
+                              datRow1("V.161"),
+                              datRow1("Repeat"),
+                              datRow1("Tidak Ada"),
+                              datRow1("S"),
+                              datRow1("0001"),
+                              datRow1("Reyner"),
+                              datRow1("Ikan Laut sALMON 70g"),
+                              datRow1("3SS + Sealed"),
+                              datRow1("1"),
+                              datRow1(""),
+                              datRow1("0"),
+
+                              datRow2("66.000", "60.000"),
+
+                              datRow1("15.180"),
+                              datRow1("0.370"),
+                              datRow1("0.280"),
+                              datRow1("0.070"),
+                              datRow1("0.740"),
+                              datRow1(""),
+                              datRow1(""),
+                              datRow1("8"),
+
+                              datRow4("PET", "15", "u", "1.16"),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("PE1", "80", "u", "0.92"),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow4("", "", "u", ""),
+                              datRow1("0.760"),
+                              datRow1(""),
+                              datRow1("-0.760"),
+
+                              datRow1("200.074"),
+                              datRow1("46.16"),
+                              datRow1("46.16"),
+                              datRow1("849.11"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow1("849.11"),
+                              datRow1("-"),
+                              datRow1("849.11"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow1("-"),
+                              datRow3()
+                            ],
+                          ),
+                        ]),
+                  ))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
--- lib/pages/widgets/textview.dart
+++ lib/pages/widgets/textview.dart
@@ -27,3 +27,203 @@ class TextView extends StatelessWidget {
     );
   }
 }
+
+datCol(value) {
+  return DataColumn(
+    label: Expanded(
+      child: Center(
+        child: TextView(
+          val: value,
+          color: darkText,
+          weight: FontWeight.w500,
+        ),
+      ),
+    ),
+  );
+}
+
+datRow1(value1) {
+  return DataCell(
+    Expanded(
+      child: Center(
+        child: TextView(
+          val: value1,
+          color: darkText,
+        ),
+      ),
+    ),
+  );
+}
+
+datRow2(value1, value2) {
+  return DataCell(
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Center(
+            child: TextView(
+              val: value1,
+              color: darkText,
+            ),
+          ),
+        ),
+        const VerticalDivider(
+          width: 20,
+          thickness: 1,
+          indent: 0,
+          endIndent: 0,
+          color: Colors.grey,
+        ),
+        Expanded(
+          child: Center(
+            child: TextView(
+              val: value2,
+              color: darkText,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+datRow3() {
+  return DataCell(
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Center(
+            child: Container(
+              height: 80,
+              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: ElevatedButton(
+                  style: TextButton.styleFrom(
+                    side: const BorderSide(
+                        width: 1, // the thickness
+                        color: Colors.black // the color of the border
+                        ),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Icon(Icons.edit, size: 20,)),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Container(
+              height: 80,
+              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: ElevatedButton(
+                  style: TextButton.styleFrom(
+                    side: const BorderSide(
+                        width: 1, // the thickness
+                        color: Colors.black // the color of the border
+                        ),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Icon(Icons.edit, size: 20,)),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: Container(
+              height: 80,
+              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: ElevatedButton(
+                  style: TextButton.styleFrom(
+                    side: const BorderSide(
+                        width: 1, // the thickness
+                        color: Colors.black // the color of the border
+                        ),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Icon(Icons.edit, size: 20,)),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+datRow4(value1, value2, value3, value4) {
+  return DataCell(
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: TextView(
+              val: value1,
+              color: darkText,
+            ),
+          ),
+        ),
+        const VerticalDivider(
+          width: 20,
+          thickness: 1,
+          indent: 0,
+          endIndent: 0,
+          color: Colors.grey,
+        ),
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: TextView(
+              val: value2,
+              color: darkText,
+            ),
+          ),
+        ),
+        const VerticalDivider(
+          width: 20,
+          thickness: 1,
+          indent: 0,
+          endIndent: 0,
+          color: Colors.grey,
+        ),
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: TextView(
+              val: value3,
+              color: darkText,
+            ),
+          ),
+        ),
+        const VerticalDivider(
+          width: 20,
+          thickness: 1,
+          indent: 0,
+          endIndent: 0,
+          color: Colors.grey,
+        ),
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: TextView(
+              val: value4,
+              color: darkText,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
